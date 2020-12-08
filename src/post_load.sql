feb652c206debe49da83f1bf34708d53fac0a8c8
danhill <danhill@digital.uc.dwp.gov.uk>
2020-12-08 20:03:53 +0000

    optimise tables after deleting rows with NULL IDs
    Signed-off-by: danhill <danhill@digital.uc.dwp.gov.uk>

--- src/post_load.sql
+++ src/post_load.sql
@@ -6,6 +6,10 @@ ALTER TABLE claimant_stage ADD CONSTRAINT citizen_id UNIQUE KEY (citiezen_id);
 ALTER TABLE contract_stage ADD CONSTRAINT contract_id UNIQUE KEY (contract_id);
 ALTER TABLE statement_stage ADD CONSTRAINT statement_id UNIQUE KEY (statement_id);
 
+OPTIMIZE TABLE claimant_stage;
+OPTIMIZE TABLE contract_stage;
+OPTIMIZE TABLE statement_stage;
+
 CREATE INDEX idx_nino on claimant_stage (nino);
 CREATE INDEX idx_citizen_id on claimant_stage (citizen_id);
 
